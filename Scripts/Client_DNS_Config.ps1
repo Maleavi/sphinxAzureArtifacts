98142e8cbc0408e2e5b3779ece765b5da8b443b3
Maleavi <manueltohl@gmail.com>
2019-10-21 15:50:25 +0200

    Add files via upload

---
+++ Scripts/Client_DNS_Config.ps1
@@ -0,0 +1,4 @@
+﻿﻿param([string]$netadapt= (Get-NetAdapter).Name, 
+    [string]$dnsip="192.168.0.1")
+
+Set-DnsClientServerAddress -InterfaceAlias $netadapt -ServerAddresses $dnsip
---
+++ Scripts/Grund_Config.ps1
@@ -0,0 +1,12 @@
+﻿﻿param($netadapt= (Get-NetAdapter).Name,
+$ip="192.168.0.1",
+$prefix=24,
+$dnsip="192.168.0.1",
+$pfad="C:\Users\Vahid\Desktop\starter.ps1")
+
+Rename-Computer -NewName "DC01"
+New-NetIPAddress -InterfaceAlias $netadapt -IPAddress $ip -PrefixLength $prefix
+Set-DnsClientServerAddress -InterfaceAlias $netadapt -ServerAddresses $dnsip
+Set-TimeZone -Id "W. Europe Standard Time"
+Enable-PSRemoting -Force
+Restart-Computer
