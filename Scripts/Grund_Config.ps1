cedf84e48023c25a1050590c24c01e2bdcf24c88
TheRoyalLion <37138130+TheRoyalLion@users.noreply.github.com>
2019-10-21 16:06:41 +0200

    Update Grund_Config.ps1

--- Scripts/Grund_Config.ps1
+++ Scripts/Grund_Config.ps1
@@ -1,10 +1,10 @@
 ﻿﻿param($netadapt= (Get-NetAdapter).Name,
 $ip="192.168.0.1",
+$hostname="DC01"
 $prefix=24,
-$dnsip="192.168.0.1",
-$pfad="C:\Users\Vahid\Desktop\starter.ps1")
+$dnsip="192.168.0.1")
 
-Rename-Computer -NewName "DC01"
+Rename-Computer -NewName $hostname
 New-NetIPAddress -InterfaceAlias $netadapt -IPAddress $ip -PrefixLength $prefix
 Set-DnsClientServerAddress -InterfaceAlias $netadapt -ServerAddresses $dnsip
 Set-TimeZone -Id "W. Europe Standard Time"
